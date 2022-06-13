aa1e76ea61fcb19c1469f883a35fce53863d592e
Marvin Parada <hello@marvin.dev>
2022-06-12 22:09:32 -0700

    RPM-0017 | Add GitHub link.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,13 +3,30 @@
     class="bg-gradient-to-b flex from-primary justify-center min-h-screen text-white to-secondary"
   >
     <div class="max-w-5xl w-full">
-      <header class="flex items-end p-4">
-        <img
-          src="@/assets/images/random_password_monster.svg"
-          alt="Random Password Monster"
-          class="animate-bounce antialiased h-16 mr-2"
-        />
-        <AppLogo />
+      <header class="flex justify-between p-4">
+        <div class="flex items-end">
+          <img
+            src="@/assets/images/random_password_monster.svg"
+            alt="Random Password Monster"
+            class="animate-bounce antialiased h-16 mr-2"
+          />
+          <AppLogo />
+        </div>
+        <div class="flex items-center">
+          <a
+            href="https://github.com/iamthismarvin/random-password-monster"
+            target="_blank"
+            ref="noopener noreferrer"
+            class="flex flex-col items-center"
+          >
+            <img
+              src="@/assets/images/github.svg"
+              alt="GitHub"
+              class="h-8 transition-all mb-2 w-8 hover:scale-105"
+            />
+            <p class="font-semibold leading-none text-sm">Open Source</p>
+          </a>
+        </div>
       </header>
       <main>
         <PasswordGenerator :options="passwordOptions" class="p-4" />
