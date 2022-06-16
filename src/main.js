e885dc3aeba615be4bbe711f4b251d4552b04a0c
Marvin Parada <hello@marvin.dev>
2022-06-15 21:35:54 -0700

    RPM-0025 | Add service worker registration.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { registerSW } from 'virtual:pwa-register'
 
 import App from './App.vue'
 import router from './router'
@@ -9,5 +10,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(registerSW())
 
 app.mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
     VitePWA({
+      registerType: 'autoUpdate',
       includeAssets: [
         'favicon.svg',
         'favicon.ico',
@@ -19,36 +20,29 @@ export default defineConfig({
         short_name: 'Random Password Monster',
         description:
           'A free to use tool to help you create safe passwords for your accounts.',
+        theme_color: '#321e55',
+        background_color: '#321e55',
         icons: [
           {
-            src: '/icons/pwa-192x192.png',
+            src: 'pwa-192x192.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: '/icons/pwa-512x512.png',
+            src: 'pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png',
           },
           {
-            src: '/icons/pwa-512x512.png',
+            src: 'pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable',
           },
-          {
-            src: '/icons/android-chrome-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: '/icons/android-chrome-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
         ],
-        theme_color: '#321e55',
-        background_color: '#321e55',
+      },
+      devOptions: {
+        enabled: true,
       },
     }),
   ],
